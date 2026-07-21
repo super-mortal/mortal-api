91efdbc7b7d9f2b0a6adf8ddc858c484e5498716
super-mortal <2169702639@qq.com>
2026-07-21 17:27:48 +0800

    chore: add exceljs/pdfkit/archiver deps, receipt+download icons, download.svg

--- scripts/download-lucide-icons.js
+++ scripts/download-lucide-icons.js
@@ -20,6 +20,8 @@ const path = require('path');
 const ICONS_DIR = path.join(__dirname, '..', 'public', 'icons');
 
 const neededIcons = [
+  "download",
+  "receipt",
   'layout-dashboard', 'key-round', 'plug', 'list', 'log-out', 'check', 'x',
   'trash-2', 'plus', 'pencil', 'copy', 'refresh-cw', 'search', 'calendar', 'filter',
   'chevron-down', 'chevron-left', 'chevron-right', 'chevron-up',
